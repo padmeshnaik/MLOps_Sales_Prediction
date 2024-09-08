b70f9e96acfb025ab22e083d6dafe9316c9029d6
padmeshnaik <padmeshnaik22@gmail.com>
2024-09-08 13:20:46 -0400

    made changes in the preprocess file

--- ml_models/preprocess.py
+++ ml_models/preprocess.py
@@ -2,5 +2,7 @@ import pandas as pd
 
 def preprocess():
     df = pd.read_csv('/tmp/train.csv')
+    
     df.fillna(0, inplace=True)
+    
     df.to_csv('/tmp/preprocessed_train.csv', index=False)
