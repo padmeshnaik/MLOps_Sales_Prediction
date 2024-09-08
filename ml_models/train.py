a16100aff4c13c9c70253cc666685d52aef3bce1
padmeshnaik <padmeshnaik22@gmail.com>
2024-09-08 19:00:07 -0400

    Testing

--- ml_models/train.py
+++ ml_models/train.py
@@ -4,8 +4,8 @@ import pandas as pd
 
 def train():
     df = pd.read_csv('/tmp/preprocessed_train.csv')
-    X = df[['feature1', 'feature2']]  # Your features
-    y = df['target']  # Your target
+    X = df[['Item_MRP', 'Item_Weight','Outlet_Establishment_Year']]  # Define your features
+    y = df['Item_Outlet_Sales'] 
 
     model = RandomForestRegressor()
     model.fit(X, y)
